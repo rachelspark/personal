5dc06b39079ecb14ee23b33554dc0ab19b93eb73
Rachel Park <rachelsupark@gmail.com>
2023-09-04 22:28:31 -0700

    adding header active

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,22 @@
 <!-- Header.svelte -->
+<script>
+    import { page } from '$app/stores';
+</script>
+
 <header class="layout-md flex flex-wrap justify-between text-lg py-8 align-start">
     <a class="text-2xl font-bold" href="/">Rachel Park</a>
-    <nav class="flex space-x-3 align-start">
-        <a href="/writing">writing</a>
-        <a href="/projects">projects</a>
-        <a href="/">about</a>
+    <nav class="flex text-black space-x-3 align-start">
+        <a href="/writing" class="hover:text-[#672468]"
+        class:active={$page.route.id === '/writing'}>writing</a>
+        <a href="/projects" class="hover:text-[#672468]"
+        class:active={$page.route.id === '/projects'}>projects</a>
+        <a href="/" class="hover:text-[#672468]"
+        class:active={$page.route.id === '/about'}>about</a>
     </nav>
 </header>
+
+<style lang="postcss">
+    a.active {
+        @apply text-[#672468];
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
     <div class="text-lg space-y-4 mt-8 mb-16">
         <p>Hi! I'm Rachel Park. </p>
         <p>I’m a Stanford student working on the future of cloud computing at <a class="link" target="_blank" href="https://modal.com/">Modal</a>.</p>
-        <p>I like building products that help people create, collaborate, and learn. Prior to Modal, I straddled product growth and engineering at <a class="link" target="_blank" href="https://www.warp.dev/">Warp</a> and <a class="link" target="_blank" href="https://get.mem.ai/"> Mem</a>.</p>
+        <p>I like building products that help people build, create, and collaborate. Prior to Modal, I straddled product growth and engineering at <a class="link" target="_blank" href="https://www.warp.dev/">Warp</a> and <a class="link" target="_blank" href="https://get.mem.ai/"> Mem</a>.</p>
         <p>In my free time, I enjoy the outdoors, iced coffee, memoirs, and volleyball.</p>
     </div>
     <div class="text-lg">
