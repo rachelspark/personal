fb20d730c79d64da22968aae596fc59c00288296
Rachel Park <rachelsupark@gmail.com>
2023-09-05 02:36:39 -0700

    remove path

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,6 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -22,9 +20,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-            base: dev ? '' : '/rachelspark.github.io',
-        }
 	}
 };
 
